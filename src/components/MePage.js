a2aa8a1642f82f52fcf5d3bd7e3c76c4893da4ad
Ryan Rickert <RyanCRickert@gmail.com>
2018-04-20 11:27:33 -0500

    Removed names

--- src/components/MePage.js
+++ src/components/MePage.js
@@ -9,7 +9,9 @@ export default () => (
   </div>
   <div className="me-page">
   <p>
-    Hi there.  I am an up and coming web developer using Node.JS and ReactJS.  I created this mockupt to show my skill of taking an existing idea and being able to replicate it.  I did this with React, PhotoSwipeReact and Sass.
+    Hi there.  I am an up and coming web developer using ReactJS with a bit of knowledge of NodeJS.  
+    I created this mockup to show my skill of taking an existing idea and being able to replicate it.  
+    I did this using React, PhotoSwipeReact and Sass.
   </p>
   <p>
   I enjoy games and my family.
